983073aa30e5d10ec4ee047ff6802296912d8273
Jonathan Handoyo <jonathan.handoyo@gmail.com>
2019-09-09 20:14:48 +0800

    Initial commit

---
+++ file-receiver/src/main/kotlin/com/example/filereceiver/FileReceiverApplication.kt
@@ -0,0 +1,27 @@
+package com.example.filereceiver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.core.io.ByteArrayResource
+import org.springframework.http.MediaType
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Mono
+import reactor.core.publisher.toMono
+
+@RestController
+@RequestMapping
+@SpringBootApplication
+class FileReceiverApplication {
+
+  @PostMapping(value = ["/upload"], consumes = [MediaType.APPLICATION_PDF_VALUE])
+  fun upload(@RequestBody pdf: ByteArrayResource): Mono<Boolean> {
+    return true.toMono()
+  }
+}
+
+fun main(args: Array<String>) {
+  runApplication<FileReceiverApplication>(*args)
+}
---
+++ file-uploader/src/main/kotlin/com/example/fileuploader/FileUploaderApplication.kt
@@ -0,0 +1,38 @@
+package com.example.fileuploader
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.event.ApplicationReadyEvent
+import org.springframework.boot.runApplication
+import org.springframework.context.event.EventListener
+import org.springframework.core.io.ByteArrayResource
+import org.springframework.core.io.Resource
+import org.springframework.core.io.ResourceLoader
+import org.springframework.stereotype.Component
+import org.springframework.web.reactive.function.BodyInserters
+import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.bodyToMono
+
+@Component
+@SpringBootApplication
+class FileUploaderApplication(val resourceLoader: ResourceLoader) {
+
+  @EventListener
+  fun onApplicationReady(event: ApplicationReadyEvent) {
+    WebClient.create("http://localhost:8081")
+      .post()
+      .uri("/upload")
+      .headers { headers ->
+        headers.set("Content-Type", "application/json")
+      }
+      .body(BodyInserters.fromResource(resourceLoader.getResource("classpath:test.pdf")))
+      .exchange()
+      .flatMap { response -> response.bodyToMono<Boolean>() }
+      .doOnNext { println(it) }
+      .subscribe()
+  }
+}
+
+fun main(args: Array<String>) {
+  runApplication<FileUploaderApplication>(*args)
+}
