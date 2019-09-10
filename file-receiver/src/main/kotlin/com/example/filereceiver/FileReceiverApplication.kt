3e49f776af7ec9e31ae37d901d9b5ed3a7c31a1c
Jonathan Handoyo <jonathan.handoyo@gmail.com>
2019-09-10 21:56:43 +0800

    Initial commit

--- file-receiver/src/main/kotlin/com/example/filereceiver/FileReceiverApplication.kt
+++ file-receiver/src/main/kotlin/com/example/filereceiver/FileReceiverApplication.kt
@@ -3,24 +3,54 @@ package com.example.filereceiver
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.core.io.ByteArrayResource
+import org.springframework.http.HttpHeaders
 import org.springframework.http.MediaType
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import reactor.core.publisher.Mono
 import reactor.core.publisher.toMono
+import java.util.*
 
 @RestController
 @RequestMapping
 @SpringBootApplication
 class FileReceiverApplication {
 
-  @PostMapping(value = ["/upload"], consumes = [MediaType.APPLICATION_PDF_VALUE])
-  fun upload(@RequestBody pdf: ByteArrayResource): Mono<Boolean> {
-    println("Content-Length: ${pdf.contentLength()}")
-    return true.toMono()
+  @PostMapping(value = ["/scan"], consumes = [MediaType.APPLICATION_JSON_VALUE])
+  fun scan(@RequestBody request: Request): Mono<Response> {
+    return when (request.contentType) {
+      MediaType.TEXT_PLAIN_VALUE -> Response(uuid(), Response.Status.OK).toMono()
+      MediaType.APPLICATION_PDF_VALUE -> Response(uuid(), Response.Status.PENDING).toMono()
+      else -> Response(uuid(), Response.Status.UNSUPPORTED).toMono()
+    }
   }
+
+  @PostMapping(value = ["/scan/{id}/upload"], consumes = [MediaType.ALL_VALUE])
+  fun upload(@RequestHeader headers: HttpHeaders, @PathVariable id: String, @RequestBody bytes: ByteArrayResource): Mono<Response> {
+    println(headers.toString())
+    return Response(uuid(), Response.Status.BLOCK).toMono()
+  }
+}
+
+fun uuid() = UUID.randomUUID().toString()
+
+data class Request(
+  val sender: Sender,
+  val contentType: String,
+  val text: String? = null,
+  val meta: Map<String, Any>? = null
+) {
+  data class Sender(
+    val name: String,
+    val email: String,
+    val pid: String
+  )
+}
+
+data class Response(
+  val id: String,
+  val status: Status
+) {
+  enum class Status { OK, INFO, BLOCK, PENDING, UNSUPPORTED }
 }
 
 fun main(args: Array<String>) {
--- file-uploader/src/main/kotlin/com/example/fileuploader/FileUploaderApplication.kt
+++ file-uploader/src/main/kotlin/com/example/fileuploader/FileUploaderApplication.kt
@@ -1,38 +1,82 @@
 package com.example.fileuploader
 
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.context.event.ApplicationReadyEvent
 import org.springframework.boot.runApplication
-import org.springframework.context.event.EventListener
-import org.springframework.core.io.ByteArrayResource
 import org.springframework.core.io.Resource
 import org.springframework.core.io.ResourceLoader
-import org.springframework.stereotype.Component
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.reactive.function.BodyInserters
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.bodyToMono
+import reactor.core.publisher.Mono
+import reactor.core.publisher.toMono
 
-@Component
+@RestController
+@RequestMapping
 @SpringBootApplication
 class FileUploaderApplication(val resourceLoader: ResourceLoader) {
 
-  @EventListener
-  fun onApplicationReady(event: ApplicationReadyEvent) {
-    val resource = resourceLoader.getResource("classpath:test.pdf")
-    println("Content-Length: ${resource.contentLength()}")
-    WebClient.create("http://localhost:8081")
-      .post()
-      .uri("/upload")
-      .headers { headers ->
-        headers.set("Content-Type", "application/pdf")
+  val client = WebClient.create("http://localhost:8081")
+
+  @GetMapping("/test") fun test(): Mono<Response> {
+    return resourceLoader.getResource("classpath:test4.exe").toMono()
+      .zipWhen { resource -> callScan(resource) }
+      .flatMap { tuple ->
+        val resource = tuple.t1
+        val response = tuple.t2
+
+        when (response.status) {
+          Response.Status.PENDING -> callUpload(response, resource)
+          else -> response.toMono()
+        }
       }
-      .body(BodyInserters.fromResource(resource))
-      .exchange()
-      .flatMap { response -> response.bodyToMono<Boolean>() }
-      .doOnNext { println(it) }
-      .subscribe()
+      .log()
   }
+
+  private fun callScan(resource: Resource): Mono<Response> {
+    return client
+      .post()
+      .uri("/scan")
+      .syncBody(
+        Request(
+          Request.Sender("First Last", "first.last@gmail.com", "m123456"),
+          "application/pdf" //TODO: how to get the content type from `Resource` that can default to `text/plain`
+        )
+      )
+      .retrieve()
+      .bodyToMono()
+  }
+
+  private fun callUpload(response: Response, resource: Resource): Mono<Response> {
+    return client
+      .post()
+      .uri("/scan/{0}/upload", response.id)
+      .body(BodyInserters.fromResource(resource))
+      .retrieve()
+      .bodyToMono()
+  }
+}
+
+data class Request(
+  val sender: Sender,
+  val contentType: String,
+  val text: String? = null,
+  val meta: Map<String, Any>? = null
+) {
+  data class Sender(
+    val name: String,
+    val email: String,
+    val pid: String
+  )
+}
+
+data class Response(
+  val id: String,
+  val status: Status
+) {
+  enum class Status { OK, INFO, BLOCK, PENDING, UNSUPPORTED }
 }
 
 fun main(args: Array<String>) {
